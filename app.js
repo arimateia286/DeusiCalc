26c4bcc24bac10ae8f3deb2554cb5fb9edb20e31
arimateia286 <arimateia286@gmail.com>
2025-06-16 12:36:12 -0300

    Refactor code formatting and improve offline alert messages in app.js; update HTML structure and add new dialog for alerts in index.html; enhance CSS styles for better responsiveness and visual consistency.

--- app.js
+++ app.js
@@ -4,35 +4,40 @@ const TIPOSTAXA = ["Cliente paga a taxa", "Vendedor paga a taxa"];
 const DIGITOS = "123456789.0=";
 
 const FORMATADOR = new Intl.NumberFormat('pt-BR', {
-	style: 'decimal',
-	maximumFractionDigits: 2,
-	minimumFractionDigits: 2,
+    style: 'decimal',
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
 });
 
 const data = new Date();
 
 let todasTaxas, taxas, valor = "0.00";
 
+const telaAviso = document.getElementById("telaAviso");
+const avisosDiv = document.getElementById("avisosDiv");
+
 if (navigator.onLine) {
-	(async () => {
-		const taxasTemp = await axios.get("https://raw.githubusercontent.com/arimateia286/DeusiCalc/main/taxas.json");
-		todasTaxas = taxasTemp.data;
-		localStorage.setItem("taxasLocal", JSON.stringify(todasTaxas));
-		const dataFormatada = new Intl.DateTimeFormat('pt-BR', {
-			dateStyle: 'short',
-			timeStyle: 'medium'
-		}).format(data);
-		localStorage.setItem("dataTaxas", JSON.stringify(dataFormatada));
-	})();
-	todasTaxas = JSON.parse(localStorage.getItem("taxasLocal"));
+    (async () => {
+        const taxasTemp = await axios.get("https://raw.githubusercontent.com/arimateia286/DeusiCalc/main/taxas.json");
+        todasTaxas = taxasTemp.data;
+        localStorage.setItem("taxasLocal", JSON.stringify(todasTaxas));
+        const dataFormatada = new Intl.DateTimeFormat('pt-BR', {
+            dateStyle: 'short',
+            timeStyle: 'medium'
+        }).format(data);
+        localStorage.setItem("dataTaxas", JSON.stringify(dataFormatada));
+    })();
+    todasTaxas = JSON.parse(localStorage.getItem("taxasLocal"));
 } else {
-	if (localStorage.getItem("taxasLocal") != null) {
-		alert("Você está offline!\nTaxas baixadas em " + JSON.parse(localStorage.getItem("dataTaxas")));
-		todasTaxas = JSON.parse(localStorage.getItem("taxasLocal"));
-	}
-	else {
-		alert("Você não tem a tabela de taxas baixada!\nConecte-se a internet para baixar a tabela mais recente e usar o aplicativo!");
-	}
+    if (localStorage.getItem("taxasLocal") != null) {
+        avisosDiv.innerHTML = "<div class='textoPopup'>Você está offline, mas as taxas foram baixadas em " + JSON.parse(localStorage.getItem("dataTaxas")) + "</div>";
+        telaAviso.showModal();
+        todasTaxas = JSON.parse(localStorage.getItem("taxasLocal"));
+    }
+    else {
+        avisosDiv.innerHTML = "<div class='textoPopup'>Você não tem a tabela de taxas baixada!\nConecte-se a internet para baixar a tabela mais recente e usar o aplicativo!</div>";
+        telaAviso.showModal();
+    }
 }
 
 let maquinaAtual = "Ton";
@@ -42,24 +47,24 @@ telaSelecaoMaquina.showModal();
 
 const botaoMaquinaPagBank = document.getElementById("maquinaPagBank");
 botaoMaquinaPagBank.addEventListener("click", () => {
-	maquinaAtual = "PagBank";
-	taxas = todasTaxas[maquinaAtual];
-	telaSelecaoMaquina.close();
-	mudarEstilo();
+    maquinaAtual = "PagBank";
+    taxas = todasTaxas[maquinaAtual];
+    telaSelecaoMaquina.close();
+    mudarEstilo();
 });
 
 const botaoMaquinaTon = document.getElementById("maquinaTon");
 botaoMaquinaTon.addEventListener("click", () => {
-	maquinaAtual = "Ton";
-	taxas = todasTaxas[maquinaAtual];
-	telaSelecaoMaquina.close();
-	mudarEstilo();
+    maquinaAtual = "Ton";
+    taxas = todasTaxas[maquinaAtual];
+    telaSelecaoMaquina.close();
+    mudarEstilo();
 });
 
 const botaoBandeira = new botaoSwitch("botaoBandeira", BANDEIRAS);
 const botaoForma = new botaoSwitch("botaoForma", FORMAS);
 botaoForma.botao.onclick = () => {
-	telaParcelamento.showModal();
+    telaParcelamento.showModal();
 }
 const botaoTipoTaxa = new botaoSwitch("botaoTipoTaxa", TIPOSTAXA);
 
@@ -68,12 +73,12 @@ visor.innerHTML = valor;
 
 const botaoLimpar = document.getElementById("botaoLimpar");
 botaoLimpar.onclick = () => {
-	if (valor.length > 1 && valor != "0.00") {
-		valor = valor.slice(0, valor.length - 1);
-	} else if (valor.length == 1) {
-		valor = "0.00";
-	}
-	visor.innerHTML = valor;
+    if (valor.length > 1 && valor != "0.00") {
+        valor = valor.slice(0, valor.length - 1);
+    } else if (valor.length == 1) {
+        valor = "0.00";
+    }
+    visor.innerHTML = valor;
 }
 
 const telaParcelamento = document.getElementById("telaParcelamento");
@@ -82,151 +87,147 @@ const listaParcelamento = document.getElementById("listaParcelamento");
 
 let listaParcelametoTemp = ""
 for (let i = 0; i < FORMAS.length; i++) {
-	if (i == 0) listaParcelametoTemp += "<div class='textoPopup'  style='border-top: solid 1px #000' onclick='parcelaAoClicar(" + i + ")'>" + FORMAS[i] + "</div>";
-	else listaParcelametoTemp += "<div class='textoPopup' onclick='parcelaAoClicar(" + i + ")'>" + FORMAS[i] + "</div>";
+    if (i == 0) listaParcelametoTemp += "<div class='textoPopup'  style='border-top: solid 1px #000' onclick='parcelaAoClicar(" + i + ")'>" + FORMAS[i] + "</div>";
+    else listaParcelametoTemp += "<div class='textoPopup' onclick='parcelaAoClicar(" + i + ")'>" + FORMAS[i] + "</div>";
 }
 listaParcelamento.innerHTML = listaParcelametoTemp;
 
-const botaoParcelamentoOk = document.getElementById("botaoParcelamentoOk");
-botaoParcelamentoOk.onclick = () => {
-	telaParcelamento.close();
-}
-
 const telaResultados = document.getElementById("telaResultados");
+const resultadosDiv = document.getElementById("resultadosDiv");
 
 const divDigitos = document.getElementById("divDigitos");
 for (let i = 0; i < DIGITOS.length; i++) {
-	let botao = document.createElement("button");
-	botao.className = "botaoPreto botaoDigito";
-	botao.innerHTML = DIGITOS[i];
-	botao.onclick = () => {
-		if (DIGITOS[i] == "=") {
-			mostrarResultados();
-		} else {
-			if (valor == "0.00") valor = "";
-			valor += DIGITOS[i];
-			visor.innerHTML = valor;
-		}
-	}
-	divDigitos.appendChild(botao);
+    let botao = document.createElement("button");
+    botao.className = "botaoPreto botaoDigito";
+    botao.innerHTML = DIGITOS[i];
+    botao.onclick = () => {
+        if (DIGITOS[i] == "=") {
+            mostrarResultados();
+        } else {
+            if (valor == "0.00") valor = "";
+            valor += DIGITOS[i];
+            visor.innerHTML = valor;
+        }
+    }
+    divDigitos.appendChild(botao);
 }
 
 const telaSobre = document.getElementById("telaSobre");
-
-const botaoSobreOk = document.getElementById("botaoSobreOk");
-botaoSobreOk.onclick = () => {
-	telaSobre.close();
-}
 
 const statusConexao = document.getElementById("statusConexao");
 statusConexao.innerHTML = (navigator.onLine) ? "Online" : "Offline (" + JSON.parse(localStorage.getItem("dataTaxas")) + ")";
 
 function mudarEstilo() {
-	const mainContainer = document.querySelector(".mainContainer");
-	mainContainer.className = "mainContainer tema" + maquinaAtual;
-	let telasPopup = document.getElementsByClassName("telaPopup");
-	telasPopup = Array.from(telasPopup);
-	telasPopup.forEach(tela => {
-		tela.className = "telaPopup telaPopup tema" + maquinaAtual;
-	});
+    const mainContainer = document.querySelector(".mainContainer");
+    mainContainer.className = "mainContainer tema" + maquinaAtual;
+    let telasPopup = document.getElementsByClassName("telaPopup");
+    telasPopup = Array.from(telasPopup);
+    telasPopup.forEach(tela => {
+        tela.className = "telaPopup telaPopup tema" + maquinaAtual;
+    });
 }
 
 function mostrarResultados() {
-	let resultados = calcular(botaoBandeira.indice, botaoForma.indice, botaoTipoTaxa.indice, valor);
-	telaResultados.innerHTML = `<div class='botaoPopup'>Concluído</div>
+    let resultados = calcular(botaoBandeira.indice, botaoForma.indice, botaoTipoTaxa.indice, valor);
+    resultadosDiv.innerHTML = `
         <div class='textoPopup'>Taxa de:<br><strong class='resultadoGrande'>R$${resultados[1]} (${resultados[6]}%)</strong></div>
         <div class='textoPopup'>Passar na máquina:<br><strong class='resultadoGrande'>R$${resultados[2]}</strong></div>
         <div class='textoPopup'>Parcelado em:<br><strong class='resultadoGrande'>${resultados[3]}x de R$${resultados[4]}</strong></div>
         <div class='textoPopup'>Você recebe:<br><strong class='resultadoGrande'>R$${resultados[5]}</strong></div>
-        <div class='botaoPopup' onclick='botaoOkAoClicar();'>Ok</div>`;
-	telaResultados.showModal();
+        `;
+    telaResultados.showModal();
 }
 
 function mostrarSobre() {
-	imgSheIs = document.getElementById("imgSheIs");
-	if (botaoForma.indice > 0) imgSheIs.src = "sheIs/1.png";
-	else imgSheIs.src = "sheIs/0.png";
-	telaSobre.showModal();
+    imgSheIs = document.getElementById("imgSheIs");
+    if (botaoForma.indice > 0) imgSheIs.src = "sheIs/1.png";
+    else imgSheIs.src = "sheIs/0.png";
+    telaSobre.showModal();
 }
 
 function mostrarSelecaoMaquina() {
-	telaSelecaoMaquina.showModal();
+    telaSelecaoMaquina.showModal();
 }
 
 function parcelaAoClicar(indice) {
-	botaoForma.indice = indice;
-	botaoForma.atualizar();
-	telaParcelamento.close();
+    botaoForma.indice = indice;
+    botaoForma.atualizar();
+    telaParcelamento.close();
 }
 
 function botaoOkAoClicar() {
-	telaResultados.close();
+    telaResultados.close();
+    telaParcelamento.close();
+    telaSobre.close();
+    telaAviso.close();
 }
 
 function miau() {
-	alert("She diz: 'Miau, krl!'");
+    alert("She diz: 'Miau, krl!'");
 }
 
 function calcular(bandeira, parcelamento_, tipoTaxa, valor) {
-	let taxa, valorFinal, parcela, taxaPaga, parcelamento;
+    let taxa, valorFinal, parcela, taxaPaga, parcelamento;
 
-	taxa = taxas[bandeira][parcelamento_];
+    taxa = taxas[bandeira][parcelamento_];
 
-	if (parcelamento_ == 0) parcelamento = 1;
-	else parcelamento = parcelamento_;
+    if (parcelamento_ == 0) parcelamento = 1;
+    else parcelamento = parcelamento_;
 
-	if (tipoTaxa == 0) {
-		valorFinal = valor * 100 / (100 - taxa);
-		parcela = valorFinal / parcelamento;
-		taxaPaga = valorFinal - valor;
-	} else {
-		taxaPaga = valor * taxa / 100;
-		valorFinal = valor - taxaPaga;
-		parcela = valor / parcelamento;
-	}
+    if (tipoTaxa == 0) {
+        valorFinal = valor * 100 / (100 - taxa);
+        parcela = valorFinal / parcelamento;
+        taxaPaga = valorFinal - valor;
+    } else {
+        taxaPaga = valor * taxa / 100;
+        valorFinal = valor - taxaPaga;
+        parcela = valor / parcelamento;
+    }
 
-	let resultadosTemp;
-	if (tipoTaxa == 0) resultadosTemp = [tipoTaxa, taxaPaga, valorFinal, parcelamento, parcela, valor, taxa];
-	else resultadosTemp = [tipoTaxa, taxaPaga, valor, parcelamento, parcela, valorFinal, taxa];
+    let resultadosTemp;
+    if (tipoTaxa == 0) resultadosTemp = [tipoTaxa, taxaPaga, valorFinal, parcelamento, parcela, valor, taxa];
+    else resultadosTemp = [tipoTaxa, taxaPaga, valor, parcelamento, parcela, valorFinal, taxa];
 
-	let resultados = [];
-	for (let i = 0; i < resultadosTemp.length; i++) {
-		if (i != 3) resultados[i] = FORMATADOR.format(resultadosTemp[i]);
-		else resultados[i] = resultadosTemp[i];
-	}
+    let resultados = [];
+    for (let i = 0; i < resultadosTemp.length; i++) {
+        if (i != 3) resultados[i] = FORMATADOR.format(resultadosTemp[i]);
+        else resultados[i] = resultadosTemp[i];
+    }
 
-	return resultados;
+    return resultados;
 }
 
 function botaoSwitch(idBotao, opcoes) {
-	this.botao = document.getElementById(idBotao);
-	this.opcoes = opcoes;
-	this.indice = 0;
+    this.botao = document.getElementById(idBotao);
+    this.opcoes = opcoes;
+    this.indice = 0;
 
-	this.botao.onclick = () => {
-		if (this.indice < this.opcoes.length - 1) this.indice++;
-		else this.indice = 0;
-		this.atualizar();
-	}
+    this.botao.onclick = () => {
+        if (this.indice < this.opcoes.length - 1) this.indice++;
+        else this.indice = 0;
+        this.atualizar();
+    }
 
-	this.atualizar = () => {
-		this.botao.innerHTML = this.opcoes[this.indice];
-	}
+    this.atualizar = () => {
+        this.botao.innerHTML = this.opcoes[this.indice];
+    }
 }
 
 window.addEventListener("keydown", (event) => {
-	if (event.key == "Enter") {
-		mostrarResultados();
-	} else if (!isNaN(event.key) && DIGITOS.includes(event.key) || event.key == ".") {
-		if (valor == "0.00") valor = "";
-		valor += event.key;
-		visor.innerHTML = valor;
-	} else if (event.key == "Backspace") {
-		if (valor.length > 1 && valor != "0.00") {
-			valor = valor.slice(0, valor.length - 1);
-		} else if (valor.length == 1) {
-			valor = "0.00";
-		}
-		visor.innerHTML = valor;
-	}
+    if (event.key == "Enter") {
+        if (telaParcelamento.open || telaResultados.open || telaSobre.open || telaAviso.open) {
+            botaoOkAoClicar();
+        } else mostrarResultados();
+    } else if (!isNaN(event.key) && DIGITOS.includes(event.key) || event.key == ".") {
+        if (valor == "0.00") valor = "";
+        valor += event.key;
+        visor.innerHTML = valor;
+    } else if (event.key == "Backspace") {
+        if (valor.length > 1 && valor != "0.00") {
+            valor = valor.slice(0, valor.length - 1);
+        } else if (valor.length == 1) {
+            valor = "0.00";
+        }
+        visor.innerHTML = valor;
+    }
 });
